dabf3f1270b2475a544918433db3a94a5adc6ead
Krzysztof Stępniak <kfs.szk@gmail.com>
2019-11-17 23:38:42 +0100

    added vuetify

--- src/components/MovieDatabase.vue
+++ src/components/MovieDatabase.vue
@@ -5,7 +5,8 @@
     <ul>
       <li>
         <a href="https://www.themoviedb.org/" target="_blank" rel="noopener"
-          >The Movie DB</a>
+          >The Movie DB</a
+        >
       </li>
     </ul>
   </div>
