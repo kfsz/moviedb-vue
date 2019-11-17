e9ab2446e3f9c355da5f3eb4876129fc9743a613
Krzysztof Stępniak <kfs.szk@gmail.com>
2019-11-17 23:29:29 +0100

    initial component creation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MovieDatabase msg="Welcome to the Movie DB" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MovieDatabase from "./components/MovieDatabase.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    MovieDatabase
   }
 };
 </script>
