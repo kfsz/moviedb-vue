6f98875590fc99d18fbb740382d14f28edd8bb24
kfsz <kfs.szk@gmail.com>
2019-11-19 04:06:21 +0100

    improved listing on smaller screens

--- src/components/MovieDatabase.vue
+++ src/components/MovieDatabase.vue
@@ -9,13 +9,13 @@
           'ma-5': $vuetify.breakpoint.mdAndUp
         }"
       >
-        <v-card shaped class="mx-auto" width="300px">
+        <v-card shaped class="mx-auto" :width="($vuetify.breakpoint.smAndDown) ? '150px' : '300px'">
           <v-img
             :src="getImgUrl(movie.poster_path)"
             class="white--text align-end"
             gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.8)"
-            height="450px"
-            width="300px"
+            :height="($vuetify.breakpoint.smAndDown) ? '225px' : '450px'"
+            :width="($vuetify.breakpoint.smAndDown) ? '150px' : '300px'"
           >
             <v-card-title v-text="movie.title"></v-card-title>
           </v-img>
@@ -27,8 +27,11 @@
 
             <v-spacer></v-spacer>
 
-            <span class="mr-2">Popularity: {{ movie.popularity }}</span>
-            <span class="mr-2">Votes: {{ movie.vote_count }}</span>
+            <div :class="($vuetify.breakpoint.smAndDown) ? 'small' : ''">
+              <span class="mr-2">Popularity: {{ movie.popularity }}</span> 
+              <br v-if="($vuetify.breakpoint.smAndDown)">
+              <span class="mr-2">Votes: {{ movie.vote_count }}</span>
+            </div>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -150,6 +153,10 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.small {
+  font-size: 65%,
 }
 
 .background {
